a6446d1d41dc2437404450868b5da9c67d1b802c
ZarcDmC01 <jessesteven26@gmail.com>
2026-06-15 13:59:13 +0200

    auto: FastAPI_UI.py

--- FastAPI_UI.py
+++ FastAPI_UI.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from Image.image_ctrler import router as image_router
 
@@ -7,3 +8,6 @@ app = FastAPI(title="API NLP¨")
 app.include_router(Image)
 app.include_router(NLP)
 
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="localhost", port=8000)
