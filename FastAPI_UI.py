b42a8107e6a90d3352b2504da83ce56e59a09158
ZarcDmC01 <jessesteven26@gmail.com>
2026-06-15 14:22:14 +0200

    auto: FastAPI_UI.py

--- FastAPI_UI.py
+++ FastAPI_UI.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 from Image.image_ctrler import router as image_router
@@ -6,29 +7,25 @@ from NLP.NLP_ctrler import router as NLP_router
 from Security.Security_ctrler import router as Security_router
 from Monitoring.Monitoring_ctrler import router as Monitor_ctrler
 
-app = FastAPI(title="API NLP¨")
+app = FastAPI(title="API NLP")
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(image_router)
 app.include_router(NLP_router)
 app.include_router(Security_router)
 app.include_router(Monitor_ctrler)
 
-# @wrapper Security
-
 @app.get('/index')
-async def index():
-    pass
-    return (render_template('index.html'))
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get('/load_image')
-async def load_image():
-    pass
-    return (render_templates('load_image.html'))
+async def load_image(request: Request):
+    return templates.TemplateResponse("load_image.html", {"request": request})
 
 @app.get('/legend')
-async def get_legend():
-    pass
-    return (render_templates('legend.html'))
+async def get_legend(request: Request):
+    return templates.TemplateResponse("legend.html", {"request": request})
 
 
 if __name__ == "__main__":
